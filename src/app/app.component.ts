1b4835b5b1eef9bafb9d9a3b8d847ea8482141ff
Arnaud Bertrand <arnaudbertrand@Arnauds-MacBook-Pro.local>
2016-12-12 08:38:23 +0100

    boot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders } from 'angularfire2';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'MyContacts';
+
+  contacts = [
+    {
+      firstName: 'John',
+      lastName: 'Smith',
+      company: 'Google',
+      picture: 'http://www.convergences.org/wp-content/uploads/2015/08/Francois-Hollande1.jpg',
+      notes: `Amazing guy blablabla`,
+    },
+    {
+      firstName: 'Oriane',
+      lastName: 'Lafond',
+      company: 'Google',
+      picture: 'http://www.convergences.org/wp-content/uploads/2015/08/Francois-Hollande1.jpg',
+      notes: `Amazing guy blablabla`,
+    },
+  ];
+
+  filter = '';
+
+  user;
+
+  constructor(public af: AngularFire) {
+    this.user = this.af.auth;
+  }
+
+  getFiltered() {
+    if (!this.filter.length) {
+      return this.contacts;
+    }
+    return this.contacts.filter(contact => contact.firstName.match(this.filter));
+  }
+
+  login() {
+    this.af.auth.login({provider: AuthProviders.Google});
+  }
+
+  logout() {
+    this.af.auth.login;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material'
+import { AngularFireModule, AuthMethods, AuthProviders } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBgY0bNe2BcqCA2HymybVnWmEEo1_w1Dgk",
+  authDomain: "contacts-a2837.firebaseapp.com",
+  databaseURL: "https://contacts-a2837.firebaseio.com",
+  storageBucket: "contacts-a2837.appspot.com",
+  messagingSenderId: "1060950974043",
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +21,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig, {
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
